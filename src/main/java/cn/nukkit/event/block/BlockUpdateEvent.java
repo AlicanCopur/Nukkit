c49ae56e6bd288a8599515e7818b5df282bafb2e
AlicanCopur <37244649+AlicanCopur@users.noreply.github.com>
2020-05-13 23:03:19 +0300

    Okay

--- src/main/java/cn/nukkit/event/block/BlockUpdateEvent.java
+++ src/main/java/cn/nukkit/event/block/BlockUpdateEvent.java
@@ -11,22 +11,12 @@ import cn.nukkit.event.HandlerList;
 public class BlockUpdateEvent extends BlockEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
-    private Block blockPrevious;
 
     public static HandlerList getHandlers() {
         return handlers;
     }
 
     public BlockUpdateEvent(Block block) {
-        this.blockPrevious = null;
         super(block);
     }
-    public BlockUpdateEvent(Block block, Block blockPrevious) {
-        this.blockPrevious = blockPrevious;
-        super(block);
-    }
-    public Block getPrevious(){
-        return this.blockPrevious;
-    }
-
 }
