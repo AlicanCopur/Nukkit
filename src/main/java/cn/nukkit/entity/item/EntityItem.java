4bd2477ed629ea26bd2d566f951127034da4b203
AlicanCopur <sknugget33@gmail.com>
2020-06-06 15:37:47 +0300

    test

--- src/main/java/cn/nukkit/entity/item/EntityItem.java
+++ src/main/java/cn/nukkit/entity/item/EntityItem.java
@@ -199,7 +199,7 @@ public class EntityItem extends Entity {
             if (this.level.getBlockIdAt((int) this.x, (int) this.boundingBox.getMaxY(), (int) this.z) == 8 || this.level.getBlockIdAt((int) this.x, (int) this.boundingBox.getMaxY(), (int) this.z) == 9) {
                 this.motionY -= this.getGravity() * -0.015; //item is fully in water or in still water
             } else if (this.isInsideOfWater()) {
-                this.motionY = this.getGravity() - 0.06; //this.motionY = this.getGravity() - 0.06; //item is going up in water, don't let it go back down too fast
+                this.motionY -= this.getGravity() * -0.015; //this.motionY = this.getGravity() - 0.06; //item is going up in water, don't let it go back down too fast
             } else {
                 this.motionY -= this.getGravity(); //item is not in water
             }
